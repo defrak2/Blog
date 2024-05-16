0d16e534b3f6034d5488dd486df8d20bd1a24df7
defrak2 <defranciscok22@gmail.com>
2024-05-16 11:26:14 -0400

    started light/dark mode button

---
+++ assets/js/blog.js
@@ -0,0 +1,16 @@
+const themeSwitcher = document.querySelector('button');
+const container = document.querySelector('body');
+
+let mode = 'light';
+
+themeSwitcher.addEventListener('click', function(){
+  if (mode === 'light') {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+  else {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+}
+)
