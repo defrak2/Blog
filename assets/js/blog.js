10878934662de445b747accc9f017ef9408e07cf
defrak2 <defranciscok22@gmail.com>
2024-05-18 07:22:25 -0400

    added logic for localStorage

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,73 @@
-const themeSwitcher = document.querySelector('button');
-const container = document.querySelector('body');
+const postCount= document.querySelector('#post-count');
+const postList= document.querySelector('#posts');
+const submitButton= document.querySelector('#submit-button');
+const postInput= document.querySelector('.post-text');
+const userName= document.querySelector('#user-name');
+const blogTitle= document.querySelector('#blog-title');
+const blogContent= document.querySelector('#blog-content');
 
-let mode = 'light';
+let blogPosts = [];
 
-themeSwitcher.addEventListener('click', function(){
-  if (mode === 'light') {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
+function storePosts() {
+  const currentPost = {
+    userName: userName.value.trim(),
+    blogTitle: blogTitle.value.trim(),
+    blogContent: blogContent.value.trim(),
+  };
+  blogPosts.push(currentPost);
+  localStorage.setItem('blogPosts', JSON.stringify(blogPosts))
+};
+
+function renderPosts() {
+  postList.innerHTML = '';
+  postCount.textContent = blogPosts.length;
+
+
+  const lastPost = JSON.parse(localStorage.getItem('currentPost'));
+
+if(lastPost !== null) {
+  document.getElementById('saved-name').innerHTML = lastPost.userName;
+  document.getElementById('saved-title').innerHTML = lastPost.blogTitle;
+  document.getElementById('saved-content').innerHTML = lastPost.blogContent;
+};
+
+
+
+  blogPosts.forEach((post, index) => {
+    const li = document.createElement('li');
+    li.textContent= `${post.userName}: ${post.blogTitle} ${post.blogContent}`
+    li.setAttribute('data-index', index);
+    postList.appendChild(li);
+  })
+};
+
+function init() {
+  const storedPosts = JSON.parse(localStorage.getItem('blogPosts'));
+  if (storedPosts !== null) {
+    blogPosts = storedPosts;
   }
-  else {
-    mode = 'light';
-    container.setAttribute('class', 'light');
-  }
+  renderPosts();
 }
-)
+
+
+
+submitButton.addEventListener('click', function(event){
+  event.preventDefault();
+  const postText= postInput.value.trim();
+
+  if (postText === '') {
+    return; 
+  }
+  const currentPost = {
+    userName: userName.value.trim(),
+    blogTitle: blogTitle.value.trim(),
+    blogContent: postText,
+  }
+  blogPosts.push(currentPost);
+  storePosts();
+  postInput.value = '';
+
+  renderPosts();
+})
+
+init();
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,16 @@
-const toggleButton = document.querySelector('#toggle-button');
-const body = document.querySelector('body');
-
+const themeSwitcher = document.querySelector('button');
+const container = document.querySelector('body');
 
 let mode = 'light';
 
-body.setAttribute('class', mode);
-
-toggleButton.addEventListener('click', function() {
+themeSwitcher.addEventListener('click', function(){
   if (mode === 'light') {
-    mode = 'light'
-  } else {
     mode = 'dark';
+    container.setAttribute('class', 'dark');
   }
-  body.setAttribute('class', mode);
-});
+  else {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+}
+);
